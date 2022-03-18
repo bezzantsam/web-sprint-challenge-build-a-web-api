ddbd36fb55a931d905250036db06ced5c218df17
Sam Bezzant <sambezzant@Sams-MBP.home>
2022-03-18 13:24:38 -0700

    24/27

--- api/server.js
+++ api/server.js
@@ -1,16 +1,22 @@
-const express = require("express");
-const server = express();
-const actionsRouter = require("./actions/actions-router");
+const express = require('express');
 
-const projectsRouter = require("./projects/projects-router");
+const server = express();
+
+const actionsRouter = require("./actions/actions-router")
+
+const projectsRouter = require("./projects/projects-router")
+
 server.use(express.json());
 
-server.use("/api/projects", projectsRouter);
+server.use("/api/projects", projectsRouter)
 
-server.use("api/actions", actionsRouter);
+server.use("/api/actions", actionsRouter)
 
-server.use("*", (req, res) => {
-  res.status(404).json({ message: `${req.method} ${req.baseUrl} not found` });
-});
-
-module.exports = server;
+server.use('*', (req, res) => {
+    res.status(404).json({ message: `${req.method} ${req.baseUrl} not found!` });
+   });
+   
+ 
+   
+ module.exports = server;
+ 
