1e2f8c01fc0d8ae8fb722144f66052a1e647ad04
Sam Bezzant <sambezzant@Sams-MacBook-Pro.local>
2022-03-18 12:59:18 -0700

    middleware

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,32 @@
-// add middlewares here related to projects
+const Project = require('./projects-model')
+
+
+const validateID = (req, res, next) => {
+    Project.get(req.params.id)
+      .then(project => {
+        if (!project) {
+          res.status(404).json({ message: `could not be retrieved with ${req.params.id}`});
+        } else {
+            req.project = project;
+            next();
+        }
+      })
+      .catch(err => console.log(err))
+  };
+  
+const validateBody = (req, res, next) => {
+    const {name, description, completed} = req.body;
+
+    if (!name || !description || !completed == null) {
+        res.status(400).json({
+            message: 'Required field is missing'
+        })
+    } else {
+        next()
+    }
+}
+module.exports = 
+{
+    validateID,
+    validateBody
+}
